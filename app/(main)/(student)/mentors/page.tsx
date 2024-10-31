b2942154416545a2c4ffe78a068c80fcb10bdf40
EdwardMelendezM <edward.melendez.mendigure@gmail.com>
2024-10-31 10:57:44 -0500

    add changes with card for mentors

---
+++ app/(main)/(student)/mentors/page.tsx
@@ -0,0 +1,47 @@
+import { auth } from "@/auth";
+import HeaderPage from "@/components/ui/header-page";
+import { getEventsPublished } from "@/features/events/actions/get-events-published";
+import EventCard from "@/features/events/components/students/event-card";
+import { getMentorsPublished } from "@/features/mentors/actions/students/get-mentors-published";
+import MentorCard from "@/features/mentors/components/students/mentor-card";
+import { getMembershipActive } from "@/features/settings/actions/get-membership-active";
+import { redirect } from "next/navigation";
+
+const MentorPage = async () => {
+
+  const session = await auth()
+  if (!session || !session.user || !session.user.id) {
+    return redirect('/')
+  }
+
+  const membershipActive = await getMembershipActive(session.user.id)
+  const mentors = await getMentorsPublished()
+  return (
+    <div className="p-6 space-y-4">
+      <HeaderPage
+        title="Mentorias"
+        description="Descubre las mentorias disponibles"
+        bgColor="bg-gradient-to-r from-blue-500 to-blue-400"
+        icon="mentor"
+        iconColor="text-white"
+      />
+      {mentors.length === 0 && (
+        <div className="text-sm p-6 text-gray-500">
+          <p>No hay mentores disponibles</p>
+        </div>
+      )}
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 px-3">
+        {mentors.map((mentor) => (
+          <MentorCard
+            key={mentor.id}
+            mentor={mentor}
+            isPremium={!!membershipActive}
+          />
+        ))}
+      </div>
+
+    </div>
+  );
+}
+
+export default MentorPage;
---
+++ features/mentors/components/students/mentor-card.tsx
@@ -0,0 +1,146 @@
+'use client'
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent
+} from "@/components/ui/card";
+import Image from "next/image";
+import { Mentor } from "@prisma/client";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { usePremium } from "@/hooks/use-premium";
+import { Badge } from "@/components/ui/badge";
+import { LinkedInLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
+
+interface EventCardProps {
+  mentor: Mentor
+  isPremium: boolean
+}
+
+const MentorCard = ({
+  mentor,
+  isPremium
+}: EventCardProps) => {
+  const [SalesPlans, openPlans] = usePremium()
+
+  const router = useRouter()
+
+  const onClick = () => {
+    if (!isPremium) {
+      openPlans()
+    } else {
+      router.push(`/events/${mentor.id}`)
+    }
+  }
+
+  return (
+    <>
+      <Card
+        className="max-w-md rounded-lg shadow-lg overflow-hidden border border-gray-200 transition-transform duration-300 hover:scale-105 hover:shadow-xl bg-white flex flex-col h-full"
+      >
+        {mentor.imageUrl && (
+          <div className="relative w-full aspect-video rounded-md overflow-auto">
+            <Image
+              fill
+              className="object-cover"
+              alt={mentor.name}
+              src={mentor.imageUrl}
+            />
+          </div>
+        )}
+        <CardHeader className="p-4">
+          <CardTitle className="">
+            <h2 className="text-xl font-semibold text-gray-800">
+              {mentor.name}
+            </h2>
+            <p className="text-gray-500 text-sm font-normal">
+              {mentor.role}
+            </p>
+            <p className="text-gray-500 text-xs font-normal underline">
+              {mentor.email}
+            </p>
+            <div className="py-3">
+              {mentor.linkedinUrl && (
+                <LinkedInLogoIcon className="rounded-full w-6 h-6 border-gray-800" />
+              )}
+              {mentor.twitterUrl && (
+                <TwitterLogoIcon className="rounded-full w-6 h-6 border-gray-800" />
+              )}
+            </div>
+          </CardTitle>
+          <CardDescription className="text-sm text-gray-500 mt-1">
+            <p className="text-gray-800 text-sm font-semibold">
+              Acerca de mi
+            </p>
+            <p className="text-gray-700 text-xs">
+              {mentor.aboutMe}
+            </p>
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent className="px-4 pb-4 flex-grow ">
+          {mentor.specialty.length > 0 && (
+            <p className="text-gray-800 text-xs font-semibold mb-1">
+              Especialidades
+            </p>
+          )}
+          {mentor.specialty.length > 0 && mentor.specialty.map((specialty) => (
+            <Badge
+              key={`speciality-${specialty}`}
+              className="mr-1 text-xs"
+              variant='draft'
+            >
+              {specialty}
+            </Badge>
+          ))}
+
+          {mentor.industry.length > 0 && (
+            <p className="text-gray-800 text-xs font-semibold mb-1 mt-5">
+              Industrias
+            </p>
+          )}
+          {mentor.industry.length > 0 && mentor.industry.map((industry) => (
+            <Badge
+              key={`industry-${industry}`}
+              className="mr-1 text-xs"
+              variant='draft'
+            >
+              {industry}
+            </Badge>
+          ))}
+
+          {mentor.idioms.length > 0 && (
+            <p className="text-gray-800 text-xs font-semibold mb-1 mt-5">
+              Idiomas
+            </p>
+          )}
+          {mentor.idioms.length > 0 && mentor.idioms.map((idioms) => (
+            <Badge
+              key={`idioms-${idioms}`}
+              className="mr-1 text-xs"
+              variant='draft'
+            >
+              {idioms}
+            </Badge>
+          ))}
+
+        </CardContent>
+        <CardFooter className="p-4 bg-gray-50 mt-auto">
+          <Button
+            onClick={onClick}
+            variant='default'
+            className="w-full"
+          >
+            Agendar un reunion
+          </Button>
+        </CardFooter>
+      </Card>
+      <SalesPlans />
+    </>
+  );
+};
+
+export default MentorCard;
