449d1f38db8d9568ebe2a1306d8001957ee104bf
EdwardMelendezM <edward.melendez.mendigure@gmail.com>
2024-10-31 22:42:32 -0500

    refactor login and register

--- features/auth/components/header.tsx
+++ features/auth/components/header.tsx
@@ -3,6 +3,7 @@
 import { Poppins } from "next/font/google"
 
 import { cn } from "@/lib/utils"
+import LogoAuth from "./logo-auth"
 
 const font = Poppins({
   subsets: ["latin"],
@@ -16,9 +17,7 @@ interface HeaderProps {
 export const Header = ({ label }: HeaderProps) => {
   return (
     <div className="w-full flex flex-col gap-y-4 items-center justify-center">
-      <h1 className={cn("text-3xl semibold", font.className)}>
-        BUSINNESS
-      </h1>
+      <LogoAuth />
       <p className="text-muted-foreground text-sm">
         {label}
       </p>
