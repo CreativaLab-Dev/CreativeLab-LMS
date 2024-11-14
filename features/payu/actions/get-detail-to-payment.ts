7c7f943dd606473f7f408613770391bc06608234
EdwardMelendezM <edward.melendez.mendigure@gmail.com>
2024-11-14 15:25:12 -0500

    add changes

--- features/payu/actions/get-detail-to-payment.ts
+++ features/payu/actions/get-detail-to-payment.ts
@@ -2,6 +2,7 @@
 
 import { auth } from "@/auth"
 import { db } from "@/lib/db"
+import { calculateMD5Props, generateSignature } from "@/lib/generate-signature"
 
 export const getDetailsToPayment = async () => {
   const session = await auth()
@@ -21,6 +22,16 @@ export const getDetailsToPayment = async () => {
 
   const paymentCount = await db.paymentOrder.count() || 0
 
+  const asignatureProps: calculateMD5Props = {
+    apiKey: process.env.PAYU_SECRET_KEY || '',
+    merchantId: '1025855',
+    reference: `${paymentCount + 1}`,
+    price: '10000',
+    currency: 'COP'
+  }
+
+  const newAsignature = generateSignature(asignatureProps)
+
   const requiredFields = {
     merchantId: '1025855',
     referenceCode: `${paymentCount + 1}`,
@@ -30,9 +41,11 @@ export const getDetailsToPayment = async () => {
     amount: '10000',
     tax: '0',
     taxReturnBase: '0',
-    signature: '',
+    signature: newAsignature,
     buyerEmail: user.email,
     telephone: '',
+    responseUrl: 'http://www.test.com/response',
+    confirmationUrl: 'http://www.test.com/confirmation',
   }
   return requiredFields
 }
---
+++ lib/generate-signature.ts
@@ -0,0 +1,20 @@
+import { MD5 } from 'crypto-js';
+
+export interface calculateMD5Props {
+  apiKey: string
+  merchantId: string
+  reference: string
+  price: string
+  currency: string
+}
+
+export const generateSignature = (data: calculateMD5Props) => {
+  try {
+    const { apiKey, merchantId, reference, price, currency } = data
+    console.log({ ...data })
+    const string = `${apiKey}~${merchantId}~${reference}~${price}~${currency}`
+    return MD5(string).toString()
+  } catch (error) {
+    return null
+  }
+}
